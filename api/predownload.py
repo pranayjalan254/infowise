0f131bc9584a5bc600827d97a86be5bb23ef0e12
Pranay <pranayjalan.work@gmail.com>
2025-09-18 10:40:28 +0000

    some fixes

--- api/predownload.py
+++ api/predownload.py
@@ -1,11 +1,7 @@
 # predownload.py
 from transformers import AutoTokenizer, AutoModelForTokenClassification
-from ollama import Ollama
 
 # 1️⃣ Preload BERT NER
 AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
-# 2️⃣ Preload Ollama phi3:latest
-ollama = Ollama()
-ollama.pull("phi3:latest")
